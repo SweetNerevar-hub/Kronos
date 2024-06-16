095d8d5415ab77cb20d70aec13be69f34438ec89
Cameron <1029194@student.sae.edu.au>
2024-06-16 16:55:00 +0800

    Tower of Paint Prototype
    + Object pickup and drop mechanics

--- Kronos/Assets/Scripts/Player/PlayerInteract.cs
+++ Kronos/Assets/Scripts/Player/PlayerInteract.cs
@@ -3,10 +3,26 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     [SerializeField] private float m_interactDistance;
+    [SerializeField] private Transform m_holdPos;
+
+    private IPickupable m_heldObject;
 
     private void Update()
     {
         SendRaycast();
+
+        if (Input.GetMouseButtonDown(1) && m_heldObject != null)
+        {
+            DropObject();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_heldObject != null)
+        {
+            m_heldObject.SetHeldPosition(m_holdPos);
+        }
     }
 
     private void SendRaycast()
@@ -24,9 +40,30 @@ public class PlayerInteract : MonoBehaviour
             {
                 if (hit.collider.tag == "Interactable")
                 {
+                    if (hit.collider.TryGetComponent(out IPickupable pickup))
+                    {
+                        PickupObject(pickup);
+                        return;
+                    }
+
                     hit.collider.GetComponent<IInteractable>().Interact();
                 }
             }
         }
+    }
+
+    private void PickupObject(IPickupable pickup)
+    {
+        if (pickup.IsValidPickUp())
+        {
+            pickup.Pickup();
+            m_heldObject = pickup;
+        }
+    }
+
+    private void DropObject()
+    {
+        m_heldObject.Drop();
+        m_heldObject = null;
     }
 }
---
+++ Kronos/Assets/Scripts/Puzzles/PaintTower/PaintTowerPlatform.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintTowerPlatform : MonoBehaviour
+{
+    private List<PaintCan> m_cans = new List<PaintCan>(3);
+
+    [SerializeField] private bool m_isFinalPlatform;
+
+    private void Update()
+    {
+        if (m_isFinalPlatform && m_cans.Count == 3)
+        {
+            bool correctOrder = m_cans[0].CanSize == CanSize.Large && m_cans[1].CanSize == CanSize.Medium && m_cans[2].CanSize == CanSize.Small;
+
+            if (correctOrder)
+            {
+                foreach (PaintCan can in m_cans)
+                {
+                    if (can.IsHeld())
+                    {
+                        return;
+                    }
+                }
+
+                print("YOU COMPLETED THE PUZZLE!");
+            }
+        }
+
+        if (m_cans.Count > 0)
+        {
+            SetValidPickUp();
+        }
+    }
+
+    private void SetValidPickUp()
+    {
+        int lastIndex = m_cans.Count - 1;
+
+        for (int i = 0; i < m_cans.Count; i++)
+        {
+            if (i == lastIndex)
+            {
+                m_cans[i].SetPickupFlag(true);
+                break;
+            }
+
+            m_cans[i].SetPickupFlag(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.TryGetComponent(out PaintCan can))
+        {
+            m_cans.Add(can);
+            can.SetInBoundsFlag(true);
+            can.SetBoundsPosition(new Vector3(transform.position.x, can.transform.position.y, transform.position.z));
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.TryGetComponent(out PaintCan can))
+        {
+            m_cans.Remove(can);
+            can.SetInBoundsFlag(false);
+        }
+    }
+}
