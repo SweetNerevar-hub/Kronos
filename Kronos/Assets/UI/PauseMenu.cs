05a8ea6fedc263da5433bb29715599da9d6cfe71
1028844 <alexmalan03@gmail.com>
2024-06-27 16:32:36 +0800

    Fixed FX filter + added blockout pause menu
    pause menu has some ui sounds as well

---
+++ Kronos/Assets/UI/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public GameObject HUD;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))   //---> for pausing with ESC
+        {
+            audioSource.PlayOneShot(audioClip);
+            pauseMenu.SetActive(true);
+            HUD.SetActive(false);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Pause()   //---> for pausing with button
+    {
+         pauseMenu.SetActive(true);
+         HUD.SetActive(false);
+         Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        HUD.SetActive(true);
+        Time.timeScale = 1f;
+    }
+    public void ReturnToSettings(int sceneID)
+    {
+        //Time.timeScale = 1f;
+        //SceneManager.LoadScene(sceneID);
+        Debug.Log("go to settings");
+    }
+
+    public void QuitToMenu(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void OnApplicationQuit()
+    {
+        //Application.Quit();
+        Debug.Log("quit");
+    }
+}
