7b0c9f40d15db35bad87699694f627d31f293881
George <george.irlgames@gmail.com>
2024-06-28 12:42:10 +0800

    Setting up dialogue in control room
    - added probuilder for some quick work
    - made temporary NPC prefab

--- Kronos/Assets/Scripts/Player/PlayerCameraLook.cs
+++ Kronos/Assets/Scripts/Player/PlayerCameraLook.cs
@@ -9,23 +9,30 @@ public class PlayerCameraLook : MonoBehaviour
     private float m_yaw;
     private const int m_clampAmount = 80;
 
+    public bool inConversation; //to tell the script when we're in conversation - George
+
     private void Start()
     {
         m_player = transform.parent;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        inConversation = false;
     }
 
     private void Update()
     {
-        m_pitch += Input.GetAxis("Mouse X") * m_sensitivity * Time.deltaTime;
-        m_yaw += Input.GetAxis("Mouse Y") * m_sensitivity * Time.deltaTime;
+        if (!inConversation) //added so I can control this from the two functions I made below - George
+        {
+            m_pitch += Input.GetAxis("Mouse X") * m_sensitivity * Time.deltaTime;
+            m_yaw += Input.GetAxis("Mouse Y") * m_sensitivity * Time.deltaTime;
 
-        SetCameraClamp();
+            SetCameraClamp();
 
-        transform.localRotation = Quaternion.Euler(-m_yaw, 0f, 0f);
-        m_player.rotation = Quaternion.Euler(0f, m_pitch, 0f);
+            transform.localRotation = Quaternion.Euler(-m_yaw, 0f, 0f);
+            m_player.rotation = Quaternion.Euler(0f, m_pitch, 0f);
+        }
     }
 
     private void SetCameraClamp()
@@ -39,5 +46,19 @@ public class PlayerCameraLook : MonoBehaviour
         {
             m_yaw = -m_clampAmount;
         }
+    }
+
+    public void ConversationStart() //added so I can pause looking around when conversation starts - George
+    {
+        inConversation = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void ConversationEnd()
+    {
+        inConversation = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
