5ba2021740a2c2d79e9a7942ec304549d095f5ab
George <george.irlgames@gmail.com>
2024-06-29 17:09:52 +0800

    Envelope Quest fully implemented!
    This took me...5 hours

--- Kronos/Assets/Scripts/Player/PlayerCameraLook.cs
+++ Kronos/Assets/Scripts/Player/PlayerCameraLook.cs
@@ -9,30 +9,24 @@ public class PlayerCameraLook : MonoBehaviour
     private float m_yaw;
     private const int m_clampAmount = 80;
 
-    public bool inConversation; //to tell the script when we're in conversation - George
 
     private void Start()
     {
         m_player = transform.parent;
 
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
-
-        //inConversation = false;
     }
 
     private void Update()
     {
-        if (!inConversation) //added so I can control this from the two functions I made below - George
-        {
-            m_pitch += Input.GetAxis("Mouse X") * m_sensitivity * Time.deltaTime;
-            m_yaw += Input.GetAxis("Mouse Y") * m_sensitivity * Time.deltaTime;
+        
+        m_pitch += Input.GetAxis("Mouse X") * m_sensitivity * Time.deltaTime;
+        m_yaw += Input.GetAxis("Mouse Y") * m_sensitivity * Time.deltaTime;
 
-            SetCameraClamp();
+        SetCameraClamp();
 
-            transform.localRotation = Quaternion.Euler(-m_yaw, 0f, 0f);
-            m_player.rotation = Quaternion.Euler(0f, m_pitch, 0f);
-        }
+        transform.localRotation = Quaternion.Euler(-m_yaw, 0f, 0f);
+        m_player.rotation = Quaternion.Euler(0f, m_pitch, 0f);
+        
     }
 
     private void SetCameraClamp()
@@ -48,17 +42,4 @@ public class PlayerCameraLook : MonoBehaviour
         }
     }
 
-    //public void ConversationStart() //added so I can pause looking around when conversation starts - George
-    //{
-    //    inConversation = true;
-    //    Cursor.lockState = CursorLockMode.None;
-    //    Cursor.visible = true;
-    //}
-
-    //public void ConversationEnd()
-    //{
-    //    inConversation = false;
-    //    Cursor.lockState = CursorLockMode.Locked;
-    //    Cursor.visible = false;
-    //}
 }
