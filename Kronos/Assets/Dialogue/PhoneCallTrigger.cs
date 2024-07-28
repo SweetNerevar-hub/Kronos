fbd67b724e64acda023895757a4bd5733332a899
George <george.irlgames@gmail.com>
2024-07-28 12:44:31 +0800

    Phone in atlas' room changed

--- Kronos/Assets/Dialogue/PhoneCallTrigger.cs
+++ Kronos/Assets/Dialogue/PhoneCallTrigger.cs
@@ -6,7 +6,7 @@ using PixelCrushers.DialogueSystem;
 public class PhoneCallTrigger : MonoBehaviour
 {
 
-    private BoxCollider bx;
+    //private BoxCollider bx;
     public int seconds;
 
     public AudioSource ringing;
@@ -15,34 +15,42 @@ public class PhoneCallTrigger : MonoBehaviour
 
     private bool ignoredCaptain;
 
+    public GameObject phoneTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(WaitForPhonecall());
-        bx = GetComponent<BoxCollider>();
-        bx.enabled = false;
-
+        //bx = GetComponent<BoxCollider>();
+        //bx.enabled = false;
+        phoneTrigger.SetActive(false);
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (ignoredCaptain && !restarted)
-        {
-            restarted = true;
-            bx.enabled = false;
-            StartCoroutine(WaitForPhonecall());
-        }
+        //if (ignoredCaptain && !restarted)
+        //{
+        //    restarted = true;
+        //    bx.enabled = false;
+        //    StartCoroutine(WaitForPhonecall());
+        //}
+
+        
     }
 
     private IEnumerator WaitForPhonecall()
     {
         yield return new WaitForSeconds(seconds);
-        bx.enabled = true;
+        //bx.enabled = true;
         ringing.Play();
+        phoneTrigger.SetActive(true);
     }
 
-    
+    public void StopRinging()
+    {
+        ringing.Stop();
+    }
+
 }
